16cc811d01b10a15341421d11d89d9154fba8d84
Marius Manea <locomotion.itservices@gmail.com>
2022-07-01 10:47:00 +0300

    implement ebpf tests on supported platforms

---
+++ src/tests/install-test-dependencies.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set +e
+
+# This script needs to be run as 'root' user or with 'sudo'
+
+DEBIAN_DEPS=( make gcc pkg-config libelf-dev clang llvm bpftool )
+UBUNTU_DEPS=( make gcc pkg-config libelf-dev clang llvm linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) )
+RPM_DEPS=( make gcc pkg-config elfutils-libelf-devel clang llvm bpftool )
+ALPINE_DEPS=( make gcc pkgconf clang llvm linux-tools linux-headers zlib-dev elfutils-dev musl-dev )
+ARCH_DEPS=( make gcc pkg-config libelf clang llvm bpf )
+SUSE_DEPS=( make gcc pkg-config libelf-devel clang llvm bpftool )
+
+if command -v apt &> /dev/null; then
+    if lsb_release -a | grep -i debian &>/dev/null; then
+        # Debian
+        echo "Installing Debian dependencies, please wait .."
+        sed -r -i 's/^deb(.*)$/deb\1 contrib non-free/g' /etc/apt/sources.list
+        apt update -y
+        apt upgrade -y
+        apt install -y "${DEBIAN_DEPS[@]}"
+	ln -s /usr/sbin/bpftool /bin/bpftool
+    else
+        # Ubuntu
+        echo "Installing Ubuntu dependencies, please wait .."
+        apt update -y
+        apt upgrade -y
+        apt install -y "${UBUNTU_DEPS[@]}"
+    fi
+elif command -v dnf &> /dev/null; then
+    # CentOS8, Alma, Rocky, Fedora
+    echo "Installing Centos8/Alma/Rocky/Fedora dependencies, please wait .."
+    dnf update -y
+    dnf upgrade -y
+    dnf install -y "${RPM_DEPS[@]}"
+elif command -v apk &> /dev/null; then
+    # Alpine
+    echo "Installing Alpine dependencies, please wait .."
+    apk update
+    apk upgrade
+    apk add "${ALPINE_DEPS[@]}"
+elif command -v pacman &> /dev/null; then
+    # Arch
+    echo "Installing Arch dependencies, please wait .."
+    pacman -S --noconfirm "${ARCH_DEPS[@]}"
+elif command -v zypper &> /dev/null; then
+    # Suse,OpenSUSE
+    echo "Installing SUSE dependencies, please wait .."
+    zypper install -y "${SUSE_DEPS[@]}"
+fi
+
+#apt update && apt -y install build-essential autoconf automake coreutils pkg-config bc libelf-dev libssl-dev clang-12 clang-tools-12 libclang-12-dev llvm-12 rsync bison flex tar xz-utils wget libbfd-dev libcap-dev linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) || true
+
+#dnf update && sudo dnf -y install clang llvm bptftool elfutils-libelf-devel autoconf automake pkg-config bc rsync && dnf -y groupinstall 'Development Tools' || true
+
+#ln -s /usr/bin/clang-12 /usr/bin/clang || true
+#ln -s /usr/bin/llvm-strip-12 /usr/bin/llvm-strip || true
+
+#mkdir -p /usr/src
+#cd /usr/src
+#wget -q https://cdn.kernel.org/pub/linux/kernel/v$(echo "$KERNEL_VERSION" | cut -f 1 -d '.').x/linux-${KERNEL_VERSION}.tar.xz
+#tar -xf linux-${KERNEL_VERSION}.tar.xz
+#make -C linux-${KERNEL_VERSION}/tools/bpf/bpftool/
+#cp linux-${KERNEL_VERSION}/tools/bpf/bpftool/bpftool /usr/bin/
--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -1,31 +1,59 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+custom_test=$1
 
 three_tests=( "cachestat" "dc" "fd" "mount" "process" "shm" "socket" "swap" "sync" "vfs" )
-one_test=( "disk" "hardirq" "oomkill" "softirq" )
+one_test=( "disk" "hardirq" "oomkill" "softirq" "filesystem" )
 
-echo "Running all tests with three options"
-for i in "${three_tests[@]}" ; do
-    {
-        echo "================  Running $i  ================"
-        echo "---> Probe: "
-        "./$i" --probe
-        echo "---> Tracepoint: "
-        "./$i" --tracepoint
-        echo "---> Trampoline: "
-        "./$i" --trampoline
+run_three_tests() {
+    local test_to_run=$1
+
+    echo "================  Running $test_to_run  ================"
+    echo "---> Probe: "
+    "./$test_to_run" --probe
+    echo "---> Tracepoint: "
+    "./$test_to_run" --tracepoint
+    echo "---> Trampoline: "
+    "./$test_to_run" --trampoline
+    echo "  "
+}
+
+run_one_test() {
+    local test_to_run=$1
+
+    echo "================  Running $test_to_run  ================"
+    if [[ "$test_to_run" == "filesystem" ]]; then
+        filesystem_list=( "nfs" "ext4" "btrfs" "xfs" )
+        for fs in "${filesystem_list[@]}" ; do
+            if grep -v nodev /proc/filesystems | grep $fs; then
+                ./filesystem --$fs
+            fi
+        done
+    else
+        "./$test_to_run"
         echo "  "
-    }  >> success.log 2>> error.log
-done
+    fi
+}
 
-echo "Running all tests with single option"
-for i in "${one_test[@]}" ; do
-    {
-        echo "================  Running $i  ================"
-        "./$i"
-        echo "  "
-    }
-done
+if [[ -n "$custom_test" ]]; then
+    if [[ " ${three_tests[*]} " =~ " ${custom_test} " ]]; then
+        run_three_tests $custom_test
+    fi
+    if [[ " ${one_test[*]} " =~ " ${custom_test} " ]]; then
+        run_one_test $custom_test
+    fi
+else
+    echo "Running all tests with three options"
+    for i in "${three_tests[@]}" ; do
+        run_three_tests $i
+    done
 
-echo "We are not running filesystem or mdflush, because they can generate error, please run them."
+    echo "Running all tests with single option"
+    for i in "${one_test[@]}" ; do
+        run_one_test $i
+    done
+    echo "We are not running filesystem or mdflush, because they can generate error, please run them."
+fi
 
-ls -lh error.log success.log
+# ./filesystem
+# ./md
