b2b622250822146997f460d71d3019afa9e448ec
Marius Manea <locomotion.itservices@gmail.com>
2022-07-05 15:11:08 +0300

    cleanup obsolete code

--- src/tests/install-test-dependencies.sh
+++ src/tests/install-test-dependencies.sh
@@ -48,17 +48,3 @@ elif command -v zypper &> /dev/null; then
     echo "Installing SUSE dependencies, please wait .."
     zypper install -y "${SUSE_DEPS[@]}"
 fi
-
-#apt update && apt -y install build-essential autoconf automake coreutils pkg-config bc libelf-dev libssl-dev clang-12 clang-tools-12 libclang-12-dev llvm-12 rsync bison flex tar xz-utils wget libbfd-dev libcap-dev linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) || true
-
-#dnf update && sudo dnf -y install clang llvm bptftool elfutils-libelf-devel autoconf automake pkg-config bc rsync && dnf -y groupinstall 'Development Tools' || true
-
-#ln -s /usr/bin/clang-12 /usr/bin/clang || true
-#ln -s /usr/bin/llvm-strip-12 /usr/bin/llvm-strip || true
-
-#mkdir -p /usr/src
-#cd /usr/src
-#wget -q https://cdn.kernel.org/pub/linux/kernel/v$(echo "$KERNEL_VERSION" | cut -f 1 -d '.').x/linux-${KERNEL_VERSION}.tar.xz
-#tar -xf linux-${KERNEL_VERSION}.tar.xz
-#make -C linux-${KERNEL_VERSION}/tools/bpf/bpftool/
-#cp linux-${KERNEL_VERSION}/tools/bpf/bpftool/bpftool /usr/bin/
