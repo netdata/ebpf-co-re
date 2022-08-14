a5cf45b508e1f68aeb69afd518ffa7df6483adfb
Thiago Marques <thiagoftsm@gmail.com>
2022-08-14 16:12:28 +0000

    update_4_threads; Change SHM function used to fill table

--- src/shm.c
+++ src/shm.c
@@ -141,6 +141,7 @@ static inline int ebpf_load_and_attach(struct shm_bpf *obj, int selector)
     return ret;
 }
 
+/* This is kept to show how to use the syscalls
 int call_syscalls()
 {
 #define SHMSZ   27
@@ -178,6 +179,25 @@ int call_syscalls()
     }
 
     return 0;
+}
+*/
+
+void shm_fill_tables(struct shm_bpf *obj)
+{
+    int fd = bpf_map__fd(obj->maps.tbl_shm);
+    uint32_t key;
+    uint64_t global_data = 64;
+    for (key = 0; key < NETDATA_SHM_END; key++) {
+        if (bpf_map_update_elem(fd, &key, &global_data, BPF_ANY))
+            fprintf(stderr, "Cannot insert key %u\n", key);
+    }
+
+    fd = bpf_map__fd(obj->maps.tbl_pid_shm);
+    netdata_shm_t apps_data = { .get = 1, .at = 1, .dt = 1, .ctl = 1};
+    for (key = 0; key < 4; key++) {
+        if (bpf_map_update_elem(fd, &key, &apps_data, BPF_ANY))
+            fprintf(stderr, "Cannot insert key %u\n", key);
+    }
 }
 
 static int shm_read_apps_array(int fd, int ebpf_nprocs)
@@ -228,15 +248,14 @@ int ebpf_shm_tests(struct btf *bf, int selector, enum netdata_apps_level map_lev
         int fd = bpf_map__fd(obj->maps.shm_ctrl);
         ebpf_core_fill_ctrl(obj->maps.shm_ctrl, map_level);
 
-        ret = call_syscalls();
+        //ret = call_syscalls();
+        shm_fill_tables(obj);
         sleep(60);
+        fd = bpf_map__fd(obj->maps.tbl_shm);
+        ret = ebpf_read_global_array(fd, ebpf_nprocs, NETDATA_SHM_END);
         if (!ret) {
-            fd = bpf_map__fd(obj->maps.tbl_shm);
-            ret = ebpf_read_global_array(fd, ebpf_nprocs, NETDATA_SHM_END);
-            if (!ret) {
-                fd = bpf_map__fd(obj->maps.tbl_pid_shm);
-                ret = shm_read_apps_array(fd, ebpf_nprocs);
-            }
+            fd = bpf_map__fd(obj->maps.tbl_pid_shm);
+            ret = shm_read_apps_array(fd, ebpf_nprocs);
         }
     } else {
         ret = 3;
