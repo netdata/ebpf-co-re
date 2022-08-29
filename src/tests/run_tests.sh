ab4bbbcc14405b44ccccb1e100b3212d90311109
Thiago Marques <thiagoftsm@gmail.com>
2022-08-29 01:44:24 +0000

    update_socket_libbpf: Adapt script to run all simulations

--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -2,18 +2,32 @@
 
 three_tests=( "cachestat" "dc" "fd" "mount" "process" "shm" "socket" "swap" "sync" "vfs" )
 one_test=( "disk" "hardirq" "oomkill" "softirq" )
+end_loop=
 
 echo "Running all tests with three options"
 for i in "${three_tests[@]}" ; do
     {
-        echo "================  Running $i  ================"
-        echo "---> Probe: "
-        "./$i" --probe
-        echo "---> Tracepoint: "
-        "./$i" --tracepoint
-        echo "---> Trampoline: "
-        "./$i" --trampoline
-        echo "  "
+        pid=$("./$i" --help | grep pid)
+        if [ -z "${pid}" ]; then
+    	    end_loop=0
+        else
+    	    end_loop=2
+        fi
+
+        for j in $(seq 0 $end_loop); do
+	    if [ -z "${pid}" ]; then
+                printf "================  Running %s without PID  ================" "${i}"
+	    else
+                printf "================  Running %s with PID GROUP %s  ================" "${i}" "${j}"
+	    fi
+            echo "---> Probe: "
+            "./$i" --probe
+            echo "---> Tracepoint: "
+            "./$i" --tracepoint
+            echo "---> Trampoline: "
+            "./$i" --trampoline
+            echo "  "
+        done
     }  >> success.log 2>> error.log
 done
 
