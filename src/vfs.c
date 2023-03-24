4c69aed6766d35594bbc1664f3db305ea9a5626f
Thiago Marques <thiagoftsm@gmail.com>
2023-03-24 12:55:11 +0000

    documentation_adjust_code: Use stack instead heap to fix aligment issue on some environments. This commit modifies vfs software.

--- src/vfs.c
+++ src/vfs.c
@@ -272,9 +272,7 @@ static inline int ebpf_load_and_attach(struct vfs_bpf *obj, int selector)
 
 static int vfs_read_apps_array(int fd, int ebpf_nprocs, uint32_t my_pid)
 {
-    struct netdata_vfs_stat_t *stored = calloc((size_t)ebpf_nprocs, sizeof(struct netdata_vfs_stat_t));
-    if (!stored)
-        return 2;
+    struct netdata_vfs_stat_t stored[ebpf_nprocs];
 
     int key, next_key;
     key = next_key = 0;
@@ -287,8 +285,6 @@ static int vfs_read_apps_array(int fd, int ebpf_nprocs, uint32_t my_pid)
 
         key = next_key;
     }
-
-    free(stored);
 
     if (counter) {
         fprintf(stdout, "Apps data stored with success. It collected %lu pids\n", counter);
