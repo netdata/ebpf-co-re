ce3e3197d31e7033f18556fc18dd3d41b3f56632
Tasos Katsoulas <tasos@netdata.cloud>
2023-04-20 13:02:15 +0300

    rework install dep
    Signed-off-by: Tasos Katsoulas <tasos@netdata.cloud>

--- src/tests/install-test-dependencies.sh
+++ src/tests/install-test-dependencies.sh
@@ -1,53 +1,119 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set +e
 
 # This script needs to be run as 'root' user or with 'sudo'
 
-DEBIAN_DEPS=( "make" "gcc" "pkg-config" "libelf-dev" "clang" "llvm" "bpftool" )
-UBUNTU_DEPS=( "make" "gcc" "pkg-config" "libelf-dev" "clang llvm linux-tools-\$(uname -r)" "linux-cloud-tools-\$(uname -r)" )
-RPM_DEPS=( "make" "gcc" "pkg-config" "elfutils-libelf-devel" "clang" "llvm" "bpftool" )
-ALPINE_DEPS=( "make" "gcc" "pkgconf" "clang" "llvm" "linux-tools" "linux-headers" "zlib-dev" "elfutils-dev" "musl-dev" )
-ARCH_DEPS=( "make" "gcc" "pkg-config" "libelf" "clang" "llvm" "bpf" )
-SUSE_DEPS=( "make" "gcc" "pkg-config" "libelf-devel" "clang" "llvm" "bpftool" )
+DEBIAN_DEPS="make gcc pkg-config libelf-dev clang llvm bpftool"
+UBUNTU_DEPS="make gcc pkg-config libelf-dev clang llvm linux-tools-generic linux-cloud-tools-generic"
+RPM_DEPS="make gcc pkg-config elfutils-libelf-devel clang llvm bpftool"
+ARCH_DEPS="make gcc pkg-config libelf clang llvm bpf"
+SUSE_DEPS="make gcc pkg-config libelf-devel clang llvm bpftool"
 
-if command -v apt &> /dev/null; then
-    if lsb_release -a | grep -i debian &>/dev/null; then
+
+# mirror functions from https://github.com/netdata/netdata/blob/master/packaging/installer/kickstart.sh
+
+str_in_list() {
+  printf "%s\n" "${2}" | tr ' ' "\n" | grep -qE "^${1}\$"
+  return $?
+}
+
+get_system_info() {
+  SYSARCH="$(uname -m)"
+
+  case "$(uname -s)" in
+    Linux)
+      SYSTYPE="Linux"
+
+      if [ -z "${SKIP_DISTRO_DETECTION}" ]; then
+        os_release_file=
+        if [ -s "/etc/os-release" ] && [ -r "/etc/os-release" ]; then
+          os_release_file="/etc/os-release"
+        elif [ -s "/usr/lib/os-release" ] && [ -r "/usr/lib/os-release" ]; then
+          os_release_file="/usr/lib/os-release"
+        else
+          warning "Cannot find usable OS release information. Native packages will not be available for this install."
+        fi
+
+        if [ -n "${os_release_file}" ]; then
+          # shellcheck disable=SC1090
+          . "${os_release_file}"
+
+          DISTRO="${ID}"
+          SYSVERSION="${VERSION_ID}"
+          SYSCODENAME="${VERSION_CODENAME}"
+        else
+          DISTRO="unknown"
+          DISTRO_COMPAT_NAME="unknown"
+          SYSVERSION="unknown"
+          SYSCODENAME="unknown"
+        fi
+      else
+        warning "Distribution auto-detection overridden by user. This is not guaranteed to work, and is not officially supported."
+      fi
+
+      supported_compat_names="debian ubuntu centos fedora opensuse ol amzn arch"
+
+      if str_in_list "${DISTRO}" "${supported_compat_names}"; then
+          DISTRO_COMPAT_NAME="${DISTRO}"
+      else
+          case "${DISTRO}" in
+          opensuse-leap) DISTRO_COMPAT_NAME="opensuse" ;;
+          cloudlinux|almalinux|rocky|rhel) DISTRO_COMPAT_NAME="centos" ;;
+          artix|manjaro|obarun) DISTRO_COMPAT_NAME="arch" ;;
+          *) DISTRO_COMPAT_NAME="unknown" ;;
+          esac
+      fi
+
+      case "${DISTRO_COMPAT_NAME}" in
+        centos|ol) SYSVERSION=$(echo "$SYSVERSION" | cut -d'.' -f1) ;;
+      esac
+      ;;
+    Darwin)
+      SYSTYPE="Darwin"
+      SYSVERSION="$(sw_vers -buildVersion)"
+      ;;
+    FreeBSD)
+      SYSTYPE="FreeBSD"
+      SYSVERSION="$(uname -K)"
+      ;;
+    *) fatal "Unsupported system type detected. Netdata cannot be installed on this system using this script." F0200 ;;
+  esac
+}
+
+
+get_system_info
+
+case $DISTRO_COMPAT_NAME in 
+    debian)
         # Debian
-        echo "Installing Debian dependencies, please wait .."
-        sed -r -i 's/^deb(.*)$/deb\1 contrib non-free/g' /etc/apt/sources.list
-        apt update -y
-        apt upgrade -y
-        apt install -y "${DEBIAN_DEPS[@]}"
-	    ln -s /usr/sbin/bpftool /bin/bpftool
-    else
+        echo "Installing Debian dependencies, please wait ..";
+        sed -r -i 's/^deb(.*)$/deb\1 contrib non-free/g' /etc/apt/sources.list;
+        apt update -y;
+        apt upgrade -y;
+        printf '%s\n' "$DEBIAN_DEPS" | xargs apt install -y
+        ln -s /usr/sbin/bpftool /bin/bpftool;;
+    ubuntu)
         # Ubuntu
-        echo "Installing Ubuntu dependencies, please wait .."
-        apt update -y
-        apt upgrade -y
-        apt install -y "${UBUNTU_DEPS[@]}"
-    fi
-elif command -v dnf &> /dev/null; then
+        echo "Installing Ubuntu dependencies, please wait ..";
+        apt update -y;
+        apt upgrade -y;
+        printf '%s\n' "$UBUNTU_DEPS" | xargs apt install -y ;;
+    centos)
     # CentOS8, Alma, Rocky, Fedora
-    echo "Installing Centos8/Alma/Rocky/Fedora dependencies, please wait .."
-    dnf update -y
-    dnf upgrade -y
-    dnf install -y "${RPM_DEPS[@]}"
-elif command -v apk &> /dev/null; then
-    # Alpine
-    echo "Installing Alpine dependencies, please wait .."
-    apk update
-    apk upgrade
-    apk add "${ALPINE_DEPS[@]}"
-elif command -v pacman &> /dev/null; then
+        echo "Installing Centos8/Alma/Rocky/Fedora dependencies, please wait ..";
+        dnf update -y;
+        dnf upgrade -y;
+        printf '%s\n' "$RPM_DEPS" | xargs dnf install -y;;
+    arch)
     # Arch
-    echo "Installing Arch dependencies, please wait .."
-    pacman -S --noconfirm "${ARCH_DEPS[@]}"
-elif command -v zypper &> /dev/null; then
+        echo "Installing Arch dependencies, please wait ..";
+        printf '%s\n' "$ARCH_DEPS" | xargs  pacman -S --noconfirm;;
+    opensuse)
     # Suse,OpenSUSE
-    echo "Installing SUSE dependencies, please wait .."
-    zypper install -y "${SUSE_DEPS[@]}"
-else
-    echo "Error: No supported package manager found." >&2
-    exit 1
-fi
+        echo "Installing SUSE dependencies, please wait ..";
+        printf '%s\n' "$SUSE_DEPS" | xargs zypper install -y;;
+    * )
+        echo "Error: No supported package manager found." >&2;
+        exit 1;;
+esac
