7bee90ca2ce56a1a116e42a180c0ee7ab1e5d952
Thiago Marques <thiagoftsm@gmail.com>
2023-05-16 00:28:14 +0000

    change_loader: Add test and probe load when it is not possible to load trampoline on kernel (swap).

--- src/swap.c
+++ src/swap.c
@@ -148,12 +148,23 @@ int ebpf_load_swap(int selector, enum netdata_apps_level map_level)
 
     obj = swap_bpf__open();
     if (!obj) {
-        fprintf(stderr, "Cannot open or load BPF object\n");
-
-        return 2;
+        goto load_error;
     }
 
     int ret = ebpf_load_and_attach(obj, selector);
+    if (ret && selector != NETDATA_MODE_PROBE) {
+        swap_bpf__destroy(obj);
+
+        obj = swap_bpf__open();
+        if (!obj) {
+            goto load_error;
+        }
+
+        selector = NETDATA_MODE_PROBE;
+        ret = ebpf_load_and_attach(obj, selector);
+    }
+
+
     if (!ret) {
         int fd = bpf_map__fd(obj->maps.swap_ctrl);
         ebpf_core_fill_ctrl(obj->maps.swap_ctrl, map_level);
@@ -177,6 +188,9 @@ int ebpf_load_swap(int selector, enum netdata_apps_level map_level)
     swap_bpf__destroy(obj);
 
     return ret;
+load_error:
+    fprintf(stderr, "Cannot open or load BPF object\n");
+    return 2;
 }
 
 int main(int argc, char **argv)
