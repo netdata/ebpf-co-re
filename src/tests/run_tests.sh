149872441fef1462d35adf6408a361c58eead419
thiagoftsm <thiagoftsm@gmail.com>
2023-07-31 00:04:02 +0000

    add_limits_socket: Modify script used to debug all software

--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -10,9 +10,9 @@ for i in "${three_tests[@]}" ; do
     {
         pid=$("./$i" --help | grep pid)
         if [ -z "${pid}" ]; then
-    	    end_loop=0
+            end_loop=0
         else
-    	    end_loop=2
+            end_loop=3
         fi
 
         for j in $(seq 0 $end_loop); do
