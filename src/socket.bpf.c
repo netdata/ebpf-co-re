1cb307b13c4a0cd9aa683af80684d99eb4783eec
thiagoftsm <thiagoftsm@gmail.com>
2023-09-05 01:17:02 +0000

    update_src_socket: Modify inet_csk_accept to identify socket source.

--- src/socket.bpf.c
+++ src/socket.bpf.c
@@ -279,6 +279,28 @@ static __always_inline int netdata_common_inet_csk_accept(struct sock *sk)
         libnetdata_update_global(&socket_ctrl, NETDATA_CONTROLLER_PID_TABLE_ADD, 1);
     }
 
+    struct inet_sock *is = inet_sk(sk);
+    netdata_socket_idx_t nv_idx = { };
+    __u16 family = set_idx_value(&nv_idx, is);
+    if (family == AF_UNSPEC)
+        return 0;
+
+    netdata_socket_t *val;
+    netdata_socket_t nv_data = { };
+
+    val = (netdata_socket_t *) bpf_map_lookup_elem(&tbl_nd_socket, &nv_idx);
+    if (val) {
+        libnetdata_update_u32(&val->external_origin, 1);
+    } else {
+        nv_data.first = bpf_ktime_get_ns();
+        nv_data.ct = nv_data.first;
+        nv_data.protocol = protocol;
+        nv_data.family = family;
+        nv_data.external_origin = 1;
+
+        bpf_map_update_elem(&tbl_nd_socket, &nv_idx, &nv_data, BPF_ANY);
+    }
+
     return 0;
 }
 
