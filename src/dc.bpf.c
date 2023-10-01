51dcc68fa86d009384a9d411f2ed562e12c020ed
thiagoftsm <thiagoftsm@gmail.com>
2023-10-01 22:51:46 +0000

    functions3: Remove release_task monitoring from dc

--- src/dc.bpf.c
+++ src/dc.bpf.c
@@ -111,23 +111,6 @@ static __always_inline int netdata_common_d_lookup(long ret)
     return 0;
 }
 
-static inline int netdata_release_task_dcstat()
-{
-    netdata_dc_stat_t *removeme;
-    __u32 key = 0;
-    if (netdata_dc_not_update_apps())
-        return 0;
-
-    removeme = netdata_get_pid_structure(&key, &dcstat_ctrl, &dcstat_pid);
-    if (removeme) {
-        bpf_map_delete_elem(&dcstat_pid, &key);
-
-        libnetdata_update_global(&dcstat_ctrl, NETDATA_CONTROLLER_PID_TABLE_DEL, 1);
-    }
-
-    return 0;
-}
-
 /***********************************************************************************
  *
  *                            DC SECTION(kprobe)
@@ -148,12 +131,6 @@ int BPF_KRETPROBE(netdata_d_lookup_kretprobe)
     return netdata_common_d_lookup(ret);
 }
 
-SEC("kprobe/release_task")
-int BPF_KPROBE(netdata_dcstat_release_task_kprobe)
-{
-    return netdata_release_task_dcstat();
-}
-
 /***********************************************************************************
  *
  *                            DC SECTION(trampoline)
@@ -171,12 +148,6 @@ int BPF_PROG(netdata_d_lookup_fexit, const struct dentry *parent, const struct q
              struct dentry *ret)
 {
     return netdata_common_d_lookup((long)ret);
-}
-
-SEC("fentry/release_task")
-int BPF_PROG(netdata_dcstat_release_task_fentry)
-{
-    return netdata_release_task_dcstat();
 }
 
 char _license[] SEC("license") = "GPL";
